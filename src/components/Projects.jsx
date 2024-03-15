4d67f80ace60cbf9d00b4d15e9df788d8a603dfb
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-03-15 15:36:24 +0700

    Add project data

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,12 @@
 import { memo } from "react";
-import { PaperContainer } from "./Styled";
+import { Grid } from "@mui/material";
 
 const Projects = () => {
-  return <PaperContainer id="projects"></PaperContainer>;
+  return (
+    <Grid container id="projects">
+      <Grid item xs={12} sm={12} md={6} lg={4} xl={4}></Grid>
+    </Grid>
+  );
 };
 
 export default memo(Projects);
